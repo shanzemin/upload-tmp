ce696bb0676d381ca8644bb45b4f268b704138ed
shanzm <szm@onyx-international.com>
2020-08-17 15:59:33 +0800

    添加测试用例

--- test/app/controller/home.test.js
+++ test/app/controller/home.test.js
@@ -1,20 +1,22 @@
-'use strict';
+'use strict'
 
-const { app, assert } = require('egg-mock/bootstrap');
+const { app, assert } = require('egg-mock/bootstrap')
 
 describe('test/app/controller/home.test.js', () => {
   it('should assert', () => {
-    const pkg = require('../../../package.json');
-    assert(app.config.keys.startsWith(pkg.name));
+    const pkg = require('../../../package.json')
+    assert(app.config.keys.startsWith(pkg.name))
 
     // const ctx = app.mockContext({});
     // yield ctx.service.xx();
-  });
+  })
 
-  it('should GET /', () => {
-    return app.httpRequest()
-      .get('/')
-      .expect('hi, egg')
-      .expect(200);
-  });
-});
+  it('should POST /upload', async () => {
+    const res = await app.httpRequest()
+      .post('/upload')
+      .field('name', 'test file')
+      .attach('file', 'test/fixtures/测试.txt')
+    assert(res.status === 200)
+    assert(res.body[0].name === '测试.txt')
+  })
+})
